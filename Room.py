70f21dc54587a9847bbc109f7edcbe4c898dc798
shinaushin <shinaushin@gmail.com>
2015-04-10 21:15:52 -0700

    Adding Room Class
    1st constructor - initializes students in the room to an empty list and
    number of students in the room to 0
    
    2nd constructor - initializes students in the room to numStudents and
    list of students to students
    
    getter method for students in room, roomType
    
    occupancy - returns how many students have been assigned to the room
    
    full - returns true if room is completely occupied
    
    addStudent - adds student to the list of students already in the room

---
+++ Room.py
@@ -0,0 +1,28 @@
+
+class Room:
+    def __init__(self):
+        self.students_ = []
+        self.numStudents_ = None
+        
+    def __init__(self, numStudents, students):
+        self.numStudents_ = numStudents
+        
+        assert(self.numStudents_ >= len(students))
+        
+        self.students_ = students
+        
+    def getStudents(self):
+        return self.students_
+    
+    def roomType(self):
+        return self.numStudnets_
+    
+    def occupancy(self):
+        return len(self.students_)
+    
+    def full(self):
+        return len(self.students_) == self.numStudents_
+    
+    def addStudent(self, student):
+        assert(len(self.students_) < self.numStudents_)
+        self.students_.append(student)
